6e9f1b90a0997cbccae7e83b9d8dadabb68f154a
Sanish Shrestha <sanishshrestha26@gmail.com>
2024-01-11 17:39:06 +0545

    2nd change to index.json

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 
 
-import express from "express";
 const bodyParser = require("body-parser");
+const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
